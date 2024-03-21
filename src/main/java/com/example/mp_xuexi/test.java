5a5faab0afe2a307a5f5bebf6fca1206c4b1c2c6
18125569408 <liuqiango1@outlook.com>
2024-03-21 13:46:04 +0800

    master commit

--- src/main/java/com/example/mp_xuexi/test.java
+++ src/main/java/com/example/mp_xuexi/test.java
@@ -13,5 +13,6 @@ public class test {
         System.out.println("hello world");
         System.out.println("hello world2");
         System.out.println("hello world3");
+        System.out.println("master test");
     }
 }
